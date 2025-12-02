18a0bffbf1204cee21212e9205dcc6ed49d6c24a
ThibautDalez <thibaut.dalez@student.kuleuven.be>
2025-12-02 13:18:56 +0100

    Betere comments

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ CRGB leds[NUM_LEDS]; // Define the array of leds
 
 void setup() {  
 
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); // Initialize the LED strip object
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); // Initialize the LED strip object, specify type, data pin, color order, and number of leds
 
 } 
 
@@ -24,7 +24,8 @@ void setup() {
 
 void loop() {  
 
-  // Turn the LED on, then pause 
+  // Turn the LED on, then pause
+  // Set even indexed LEDs to cyan and odd indexed LEDs to yellow
   for (int i = 0; i < NUM_LEDS; i+=2)
   {
     leds[i] = CRGB(0,100,100);
