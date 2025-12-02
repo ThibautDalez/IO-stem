0d0c026f20fa7bc69953376e88dd3650913468e0
ThibautDalez <thibaut.dalez@student.kuleuven.be>
2025-12-02 13:08:15 +0100

    1st commit, basic code that makes the ledstrip work

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h> 
+#include <FastLED.h> 
+
+ 
+#define NUM_LEDS 16 // How many leds in your strip? 
+
+// For led chips like WS2812, which have a data line, ground, and power, you just 
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock, 
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN 
+// Clock pin only needed for SPI based chipsets when not using hardware SPI 
+
+#define DATA_PIN 3
+
+CRGB leds[NUM_LEDS]; // Define the array of leds 
+
+
+void setup() {  
+
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); // Initialize the LED strip object
+
+} 
+
+ 
+
+void loop() {  
+
+  // Turn the LED on, then pause 
+  for (int i = 0; i < NUM_LEDS; i+=2)
+  {
+    leds[i] = CRGB(0,100,100);
+  }
+    for (int i = 1; i < NUM_LEDS; i+=2)
+  {
+    leds[i] = CRGB(100,100,0);
+  }
+  FastLED.show(); 
+  delay(500); 
+
+   
+  // Now turn the LED off, then pause 
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Black; 
+  }
+  FastLED.show(); 
+  delay(500); 
+
+} 
